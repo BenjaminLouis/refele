393c663d25590eeb9ad4eee467638b63d75885cc
Benjamin Louis <benjaminlouis.pro@gmail.com>
2019-03-26 11:38:57 +0100

    updates DESCRIPTION

--- R/devstuffs_history.R
+++ R/devstuffs_history.R
@@ -1,1 +1,55 @@
+# Hide this file from build
 usethis::use_build_ignore("devstuffs_history.R")
+
+# DESCRIPTION
+library(desc)
+# Create and clean desc
+my_desc <- description$new("DESCRIPTION")
+# Set your package name
+my_desc$set("Package", "refele")
+#Set your name
+my_desc$set("Authors@R", "person('Benjamin', 'Louis', email = 'contact@benjaminlouis-stat.fr', role = c('aut', 'cre'))")
+# Remove some author fields
+my_desc$del("Maintainer")
+# Set the version
+my_desc$set_version("0.0.0.9000")
+# The title of your package
+my_desc$set(Title = "refele : a package for the EFELE project")
+# The description of your package
+my_desc$set(Description = "This package contains several functions to import, tidy and model data from the EFELE project")
+# The urls
+my_desc$set("URL", "https://github.com/BenjaminLouis/refele")
+my_desc$set("BugReports", "https://github.com/BenjaminLouis/refele/issues")
+# Depends
+my_desc$set(Depends = "R (>= 3.5.0)")
+# Save everyting
+my_desc$write(file = "DESCRIPTION")
+
+# Licence
+usethis::use_mit_license(name = "Benjamin Louis")
+
+# README + badges and CoC
+usethis::use_readme_md()
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge("Experimental")
+usethis::use_news_md()
+
+# For tests
+#usethis::use_testthat()
+#usethis::use_test("app")
+
+# Reorder your DESC
+attachment::att_to_description()
+usethis::use_tidy_description()
+
+# Vignette
+#usethis::use_vignette("wimpress")
+#devtools::build_vignettes()
+
+# Versionning
+usethis::use_git()
+
+# Codecov
+usethis::use_travis()
+usethis::use_appveyor()
+usethis::use_coverage()
